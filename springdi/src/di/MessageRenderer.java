bfb9d303f1e00d82dfc0e73fcca2eb4c2070ea89
Keith Mannock <keith@dcs.bbk.ac.uk>
2016-01-10 20:18:11 +0000

    SpringDI examples

---
+++ springdi/src/di/MessageRenderer.java
@@ -0,0 +1,10 @@
+package di;
+
+
+public interface MessageRenderer {
+
+    void render();
+    
+    void setMessageProvider(MessageProvider provider);
+    MessageProvider getMessageProvider();
+}
---
+++ springdi/src/helloworld/MessageRenderer.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public interface MessageRenderer {
+
+    void render();
+    
+    void setMessageProvider(MessageProvider provider);
+    MessageProvider getMessageProvider();
+}
