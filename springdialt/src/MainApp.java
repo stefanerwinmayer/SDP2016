8cac546e9319de572e777ade3fd890908c827d15
Keith Mannock <keith@dcs.bbk.ac.uk>
2016-01-13 14:12:16 +0000

    Source code for Java Design Pattern Essentials - Second Edition

--- springdialt/src/MainApp.java
+++ springdialt/src/MainApp.java
@@ -4,9 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-
         TextEditor te = (TextEditor) context.getBean("textEditor");
-
         te.spellCheck();
     }
 }
