9b7db4276c7e72cd30ada32c7f5ebda8e0d86630
Keith Mannock <keith@dcs.bbk.ac.uk>
2016-01-20 17:00:20 +0000

    java8 examples

---
+++ java8/src/debug/Main.java
@@ -0,0 +1,11 @@
+package debug;
+
+public class Main implements Debuggable {
+    int a = 100;
+    String b = "Home";
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        System.out.println(m.debug());
+    }
+}
