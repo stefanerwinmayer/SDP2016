8324fc8c1fe33f9bd834149a94c8c74037f23f07
Keith Mannock <keith@dcs.bbk.ac.uk>
2016-01-21 13:25:54 +0000

    cw2 java source code

---
+++ cw-pair-java/src/Game.java
@@ -0,0 +1,7 @@
+public interface Game {
+    /**
+     * Run a one or more game sof mastermind, until the player
+     * quits.
+     */
+    void runGames();
+}
---
+++ cw-pair-java/src/MastermindDriver.java
@@ -0,0 +1,11 @@
+public class MastermindDriver {
+
+    // Example - change as you need to...
+    public static void main(String[] args) {
+        Game g = Factory.getInstance(Game.class, true);
+        g.runGames();
+
+        g = Factory.getInstance(Game.class, false);
+        g.runGames();
+    }
+}
