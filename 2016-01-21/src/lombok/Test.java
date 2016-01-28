b4fb32bfd61359cfb55cc96b32d7ac48c9010c42
Keith Mannock <keith@dcs.bbk.ac.uk>
2016-01-28 20:47:44 +0000

    Exercises

--- 2016-01-21/src/lombok/Test.java
+++
@@ -1,8 +0,0 @@
-package lombok;
-
-public class Test {
-    public static void main(String[] args) {
-        Student s = new Student();
-        System.out.println(s.getSexy());
-    }
-}
---
+++ 2016-01-21/src/lombokexample/Test.java
@@ -0,0 +1,11 @@
+package lombokexample;
+
+public class Test {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setSexy("Transgender");
+        s.setId(666);
+        s.setName("Justin");
+        System.out.println(s);
+    }
+}
