1a52cdb5112baef6696b57260875775946427f9d
Keith Mannock <keith@dcs.bbk.ac.uk>
2016-01-28 19:08:27 +0000

    Scala examples from Jan 28th

---
+++ 2016-01-28/src/Fred.sc
@@ -0,0 +1,13 @@
+object Thing extends App {
+  println("asdad")
+}
+
+3 + 3
+
+"asgdaghsd" take 3
+
+val x: Array[String]
+
+x(0) = "sdasda"
+
+println(x)
---
+++ 2016-01-28/src/observer/Test.scala
@@ -0,0 +1,16 @@
+package observer
+
+object Test extends App {
+
+  def meth(s : String, i : Int): (String,Int) = {
+    var str = s
+    var j = i
+    for (x <- 0 until j){
+      str += s;
+    }
+    j = 6
+    (str,j)
+  }
+
+  println(meth("abc",3))
+}
