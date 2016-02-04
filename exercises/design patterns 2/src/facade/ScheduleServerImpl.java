0c13cea80fe7b5d1a6a92ca8d1627c6d53bfd84f
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-04 19:19:56 +0000

    Implemented the schedule server

---
+++ exercises/design patterns 2/src/facade/ScheduleServerImpl.java
@@ -0,0 +1,43 @@
+package facade;
+
+/**
+ * Created by smayer04 on 04/02/2016.
+ */
+public class ScheduleServerImpl implements ScheduleServer {
+
+    @Override
+    public void startBooting(){}
+
+    @Override
+    public void readSystemConfigFile(){}
+
+    @Override
+    public void init(){}
+
+    @Override
+    public void initializeContext(){}
+
+    @Override
+    public void initializeListeners(){}
+
+    @Override
+    public void createSystemObjects(){}
+
+    @Override
+    public void releaseProcesses(){}
+
+    @Override
+    public void destory(){}
+
+    @Override
+    public void destroySystemObjects(){}
+
+    @Override
+    public void destoryListeners(){}
+
+    @Override
+    public void destoryContext(){}
+
+    @Override
+    public void shutdown(){}
+}
