8970b949a69d605268dfe0ec0eeb97e71a05deaf
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-04 16:40:01 +0000

    Improved line spacing.

--- exercises/design patterns 1/src/decorator/TestDecoratorPattern.java
+++ exercises/design patterns 1/src/decorator/TestDecoratorPattern.java
@@ -15,16 +15,22 @@ public class TestDecoratorPattern {
         Pizza pizza = new SimplyVegPizza();
         pizza = new RomaTomatoes(pizza);
         print(pizza);
+
         pizza = new GreenOlives(pizza);
         print(pizza);
+
         pizza = new Spinach(pizza);
         print(pizza);
+
         pizza = new SimplyNonVegPizza();
         print(pizza);
+
         pizza = new Meat(pizza);
         print(pizza);
+
         pizza = new Cheese(pizza);
         print(pizza);
+
         pizza = new Ham(pizza);
         print(pizza);
     }
@@ -33,3 +39,9 @@ public class TestDecoratorPattern {
         System.out.println("Price: " + dformat.format(pizza.getPrice()));
     }
 }
+/* Output:
+Desc: SimplyVegPizza (230), Roma Tomatoes (5.20), Green Olives (5.47), Spinach (7.92)
+Price: 248.59
+Desc: SimplyNonVegPizza (350), Meat (14.25), Cheese (20.72), Cheese (20.72), Ham (18.12)
+Price: 423.81
+*/
