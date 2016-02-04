4d3df307c99f510e1f9e2d8845527fad3263dd3e
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-04 18:56:18 +0000

    Implemented concrete formatters

--- exercises/design patterns 2/src/strategy/CapTextFormatter.java
+++ exercises/design patterns 2/src/strategy/CapTextFormatter.java
@@ -6,6 +6,6 @@ package strategy;
 public class CapTextFormatter implements TextFormatter {
     @Override
     public void format(String text) {
-
+        System.out.println("[CapTextFormatter]: " + text.toUpperCase());
     }
 }
--- exercises/design patterns 2/src/strategy/LowerTextFormatter.java
+++ exercises/design patterns 2/src/strategy/LowerTextFormatter.java
@@ -6,6 +6,6 @@ package strategy;
 public class LowerTextFormatter implements TextFormatter {
     @Override
     public void format(String text) {
-
+        System.out.println("[LowerTextFormatter]: " + text.toLowerCase());
     }
 }
