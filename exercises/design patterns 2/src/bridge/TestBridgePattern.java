cecb401ee4fa6bd394fd3270222ed3b24c250116
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-08 14:50:07 +0000

    Added output comment

--- exercises/design patterns 2/src/bridge/TestBridgePattern.java
+++ exercises/design patterns 2/src/bridge/TestBridgePattern.java
@@ -24,3 +24,22 @@ public class TestBridgePattern {
         car.printDetails();
     }
 }
+
+/*
+Producing Central Locking System
+Modifying product Central Locking System according to BigWheel xz model
+Assembling Central Locking System for BigWheel xz model
+Car: BigWheel xz model, Product:Central Locking System
+Producing Gear Locking System
+Modifying product Gear Locking System according to BigWheel xz model
+Assembling Gear Locking System for BigWheel xz model
+Car: BigWheel xz model, Product:Gear Locking System
+Producing Central Locking System
+Modifying product Central Locking System according to Motoren lm model
+Assembling Central Locking System for Motoren lm model
+Car: Motoren lm model, Product:Central Locking System
+Producing Gear Locking System
+Modifying product Gear Locking System according to Motoren lm model
+Assembling Gear Locking System for Motoren lm model
+Car: Motoren lm model, Product:Gear Locking System
+ */
