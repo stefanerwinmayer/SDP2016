a48e224a7ec988fbedd5f270ecca50b9e74ef87e
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-08 16:11:25 +0000

    Eliminated compile error

--- 2016-02-06/src/tutorialcode/ComplexNumbers.scala
+++ 2016-02-06/src/tutorialcode/ComplexNumbers.scala
@@ -3,6 +3,6 @@ package tutorialcode
 object ComplexNumbers {
   def main(args: Array[String]) {
     val c = new Complex(1.2, 3.4)
-    println("imaginary part: " + c.im())
+    //println("imaginary part: " + c.im()) Compile error
   }
 }
