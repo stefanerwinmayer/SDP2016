84c10a7fe18ca0e75322fef454e19438fdea0a45
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-11 13:52:11 +0000

    Implemented the London and NYC parsers

---
+++ exercises/design patterns 2/src/abstractfactory/LondonFEEDBACK.java
@@ -0,0 +1,10 @@
+package abstractfactory;
+
+/**
+ * Created by stefanm on 11/02/2016.
+ */
+public class LondonFEEDBACK implements XMLParser {
+    public String parse() {
+        return "London Parsing feedback XML...\nLondon Feedback XML Message";
+    }
+}
---
+++ exercises/design patterns 2/src/abstractfactory/NYCORDER.java
@@ -0,0 +1,10 @@
+package abstractfactory;
+
+/**
+ * Created by stefanm on 11/02/2016.
+ */
+public class NYCORDER implements XMLParser {
+    public String parse() {
+        return "NYC Parsing order XML...\nNYC Order XML Message";
+    }
+}
