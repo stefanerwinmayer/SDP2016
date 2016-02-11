7caf21fdf4b214404be3aef4cb08717014241a6c
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-11 18:14:06 +0000

    Implemented forecast method.

---
+++ exercises/scala 2/src/forecast/MyForecastTest.scala
@@ -0,0 +1,20 @@
+package forecast
+
+import com.atomicscala.AtomicTest._
+
+object MyForecastTest extends App {
+  forecast(100) is "Sunny"
+  forecast(80) is "Mostly Sunny"
+  forecast(50) is "Partly Sunny"
+  forecast(20) is "Mostly Sunny"
+  forecast(0) is "Cloudy"
+  forecast(15) is "Unknown"
+  def forecast(temp: Int): String = temp match {
+    case 100 => "Sunny"
+    case 80 => "Mostly Sunny"
+    case 50 => "Partly Sunny"
+    case 20 => "Mostly Sunny"
+    case 0 => "Cloudy"
+    case _ => "Unknown"
+  }
+}
