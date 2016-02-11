5b20f9d5b9ac70ee1c14a368c9f74e3a89c90df5
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-11 13:42:39 +0000

    Added comment with output messages

--- exercises/design patterns 2/src/abstractfactory/TestAbstractFactoryPattern.java
+++ exercises/design patterns 2/src/abstractfactory/TestAbstractFactoryPattern.java
@@ -19,3 +19,11 @@ public class TestAbstractFactoryPattern {
     }
 
 }
+
+/* Output
+NYC Parsing order XML...
+NYC Order XML Message
+************************************
+London Parsing feedback XML...
+London Feedback XML Message
+ */
