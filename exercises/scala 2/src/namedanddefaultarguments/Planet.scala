8eb9c5667108ab9995760020909bd5340e1740b0
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-15 11:45:11 +0000

    Implemented 'Planet' class

---
+++ exercises/scala 2/src/namedanddefaultarguments/Planet.scala
@@ -0,0 +1,16 @@
+package namedanddefaultarguments
+
+import com.atomicscala.AtomicTest._
+
+/**
+  * Created by stefanm on 15/02/2016.
+  */
+class Planet(name: String, description: String, moons: Int = 0) {
+  def hasMoon = moons > 0
+}
+
+object TestPlanet extends App {
+  val p = new Planet(name = "Mercury",
+    description = "small and hot planet", moons = 0)
+  p.hasMoon is false
+}
