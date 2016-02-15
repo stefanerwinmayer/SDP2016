48f861284de089cda72f7e8f77ef8debae66687b
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-15 11:46:08 +0000

    Updated tests for class 'Planet'

--- exercises/scala 2/src/namedanddefaultarguments/Planet.scala
+++ exercises/scala 2/src/namedanddefaultarguments/Planet.scala
@@ -13,4 +13,8 @@ object TestPlanet extends App {
   val p = new Planet(name = "Mercury",
     description = "small and hot planet", moons = 0)
   p.hasMoon is false
+
+  val earth = new Planet(moons = 1, name = "Earth",
+    description = "a hospitable planet")
+  earth.hasMoon is true
 }
