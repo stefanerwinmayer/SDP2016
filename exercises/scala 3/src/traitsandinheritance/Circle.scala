028ecd31c43c843313abc941a565bd033e95b6ce
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-22 12:29:34 +0000

    Implemented circle functions

--- exercises/scala 3/src/traitsandinheritance/Circle.scala
+++ exercises/scala 3/src/traitsandinheritance/Circle.scala
@@ -4,9 +4,9 @@ package traitsandinheritance
   * Created by stefanm on 22/02/2016.
   */
 class Circle(val radius: Double) extends Shape {
-  def sides(): Double = ???
+  def sides(): Double = 0
 
-  def perimeter(): Double = ???
+  def perimeter(): Double = 2 * math.Pi * radius
 
-  def area(): Double = ???
+  def area(): Double = 2 * math.Pi * radius * radius
 }
