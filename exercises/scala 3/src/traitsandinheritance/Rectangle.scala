86aca9e0e753f7433984d2fc47cfbb96452a8dfd
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-22 12:30:39 +0000

    Implemented Rectangle  functions

--- exercises/scala 3/src/traitsandinheritance/Rectangle.scala
+++ exercises/scala 3/src/traitsandinheritance/Rectangle.scala
@@ -4,9 +4,9 @@ package traitsandinheritance
   * Created by stefanm on 22/02/2016.
   */
 class Rectangle(val x: Double, val y: Double) extends Shape {
-  def sides(): Double = ???
+  def sides(): Double = 4
 
-  def perimeter(): Double = ???
+  def perimeter(): Double = 2 * x + 2 * y
 
-  def area(): Double = ???
+  def area(): Double = x * y
 }
