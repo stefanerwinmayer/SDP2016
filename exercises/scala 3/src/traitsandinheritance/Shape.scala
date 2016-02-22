6249ca41e92743fb7ba45e861d0a85a8ca167c40
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-22 12:22:54 +0000

    Implemented trait 'Shape' and skeleton implements of various shapes

---
+++ exercises/scala 3/src/traitsandinheritance/Shape.scala
@@ -0,0 +1,12 @@
+package traitsandinheritance
+
+/**
+  * Created by stefanm on 22/02/2016.
+  */
+trait Shape {
+  def sides(): Int
+
+  def perimeter(): Int
+
+  def area(): Int
+}
---
+++ exercises/scala 3/src/traitsandinheritance/Square.scala
@@ -0,0 +1,12 @@
+package traitsandinheritance
+
+/**
+  * Created by stefanm on 22/02/2016.
+  */
+class Square(val side: Int) extends Shape {
+  def sides(): Int = ???
+
+  def perimeter(): Int = ???
+
+  def area(): Int = ???
+}
