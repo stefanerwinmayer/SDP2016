93a5d721cf638d895dc81947ea88b7d9bad5b0f8
stefanerwinmayer <stefan.e.mayer@gmail.com>
2016-02-25 10:51:58 +0000

    Implemented Colour trait and updated classes to draw with colours.

--- exercises/scala 3/src/traitsandinheritance/sealedtraits/SealedShape.scala
+++ exercises/scala 3/src/traitsandinheritance/sealedtraits/SealedShape.scala
@@ -4,14 +4,17 @@ package traitsandinheritance.sealedtraits
   * Created by stefanm on 22/02/2016.
   */
 sealed trait Shape {
+
   def sides(): Double
 
   def perimeter(): Double
 
   def area(): Double
+
+  val colour: Colour
 }
 
-case class Circle(radius: Double) extends Shape {
+case class Circle(radius: Double, colour: Colour) extends Shape {
   def sides(): Double = 1
 
   def perimeter(): Double = 2 * math.Pi * radius
@@ -30,24 +33,32 @@ sealed trait Rectangular extends Shape {
   def area(): Double = x * y
 }
 
-case class Rectangle(val x: Double, val y: Double) extends Rectangular
+case class Rectangle(val x: Double, val y: Double, colour: Colour) extends Rectangular
 
-case class Square(val side: Double) extends Rectangular {
+case class Square(val side: Double, colour: Colour) extends Rectangular {
   val x = side
   val y = side
 }
 
 object Draw {
   def apply(shape: Shape) = shape match {
-    case Circle(radius) => println(s"A circle of radius ${radius}cm")
-    case Rectangle(x, y) => println(s"A rectangle of width ${x}cm and height ${y}cm")
-    case Square(side) => println(s"A square of width and height ${side}cm")
+    case Circle(radius, colour) => println(s"A ${getColour(colour)} circle of radius ${radius}cm")
+    case Rectangle(x, y, colour) => println(s"A ${getColour(colour)} rectangle of width ${x}cm and height ${y}cm")
+    case Square(side, colour) => println(s"A ${getColour(colour)} square of width and height ${side}cm")
+  }
+
+  def getColour(colour: Colour) = colour match {
+    case Red => "red"
+    case Yellow => "yellow"
+    case Pink => "pink"
+    case CustomColor(r, g, b) => if (colour.isDark) "dark" else "light"
   }
 }
 
 object  Main extends App {
-  Draw(Circle(10)) // returns "A circle of radius 10cm"
-  Draw(Rectangle(3, 4)) // returns "A rectangle of width 3cm and height 4cm"
+  Draw(Circle(10, Red)) // returns "A circle of radius 10cm"
+  Draw(Rectangle(3, 4, Pink)) // returns "A rectangle of width 3cm and height 4cm"
+  Draw(Square(6, CustomColor(255, 255, 255)))
 }
 
 sealed trait Colour {
