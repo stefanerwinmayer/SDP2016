22fb6a2748555ba33b1f16b1e49e86f7d1013e9c
Keith Mannock <keith@dcs.bbk.ac.uk>
2016-03-01 15:12:37 +0000

    scala fx examples

---
+++ scalafx/src/exampleone/Example.scala
@@ -0,0 +1,25 @@
+package exampleone
+
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
+
+object HelloStageDemo extends JFXApp {
+  stage = new JFXApp.PrimaryStage {
+    title.value = "Hello Stage"
+    width = 600
+    height = 450
+    scene = new Scene {
+      fill = Color.LightGreen
+      content = new Rectangle {
+        x = 25
+        y = 40
+        width = 100
+        height = 100
+        fill <== when (hover) choose Color.Green otherwise Color.Red
+      }
+    }
+  }
+}
